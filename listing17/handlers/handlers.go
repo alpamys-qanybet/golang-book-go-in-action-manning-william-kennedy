0afdb0cd28d2bb490f87c0e6268f37cb53162e68
Alpamys Kanibetov <alpamys.kanibetov@gmail.com>
2022-02-03 02:01:24 +0600

    server handler json

--- listing17/handlers/handlers.go
+++ listing17/handlers/handlers.go
@@ -1,11 +1,25 @@
 package handlers
 
 import (
-  "fmt"
-  // "encoding/json"
-  // "net/http"
+  "encoding/json"
+  "net/http"
 )
 
 func Routes() {
-  fmt.Println("Routes")
+  http.HandleFunc("/sendjson", SendJSON)
+}
+
+func SendJSON(rw http.ResponseWriter, r *http.Request) {
+  u := struct {
+    Name string
+    Email string
+  } {
+    Name: "Bill",
+    Email: "bill@gmail.com",
+  }
+
+  rw.Header().Set("Content-Type", "application/json")
+  rw.WriteHeader(http.StatusOK)
+
+  json.NewEncoder(rw).Encode(&u)
 }
