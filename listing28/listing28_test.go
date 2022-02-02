08fb346c27235e3ae7117fd7f814386daa469f07
Alpamys Kanibetov <alpamys.kanibetov@gmail.com>
2022-02-03 04:56:33 +0600

    bench #2

--- listing28/listing28_test.go
+++ listing28/listing28_test.go
@@ -2,7 +2,7 @@ package listing28_test
 
 import (
   "fmt"
-  // "strconv"
+  "strconv"
   "testing"
 )
 
@@ -18,3 +18,27 @@ func BenchmarkSprintf(b *testing.B) {
 
 // go test -v -run="none" -bench="BenchmarkSprintf"
 // go test -v -run="none" -bench="BenchmarkSprintf" -benchtime="3s"
+
+func BenchmarkFormat(b *testing.B) {
+  n := int64(10)
+
+  b.ResetTimer()
+
+  for i := 0; i < b.N; i++ {
+    strconv.FormatInt(n, 10)
+  }
+}
+
+func BenchmarkItoa(b *testing.B) {
+  n := 10
+
+  b.ResetTimer()
+
+  for i := 0; i < b.N; i++ {
+    strconv.Itoa(n)
+  }
+}
+
+// go test -v -run="none" -bench=.
+// go test -v -run="none" -bench=. -benchtime="3s"
+// go test -v -run="none" -bench=. -benchmem
