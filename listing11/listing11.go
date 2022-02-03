65275c8c6fce77a5564c502bf0cb3d6914290712
Alpamys Kanibetov <alpamys.kanibetov@gmail.com>
2022-02-03 11:35:47 +0600

    value vs ref

---
+++ listing11/listing11.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "fmt"
+)
+
+type user struct {
+  name  string
+  email string
+}
+
+func (u user) notify() {
+  fmt.Printf("Sending user email to %s<%s>\n", u.name, u.email)
+}
+
+func (u *user) changeEmail(email string) {
+  u.email = email
+}
+
+func main() {
+  bill := user{"Bill", "bill@gmail.com"}
+  bill.notify()
+
+  lisa := &user{"Lisa", "lisa@gmail.com"}
+  lisa.notify()
+
+  bill.changeEmail("bill@yahoo.com")
+  bill.notify()
+
+  lisa.changeEmail("lisa@newdomain.com")
+  lisa.notify()
+}
